e9fc1d9d7b5c73676c87049c5b19c294c30d5fe5
Hugh Stimson <hugh@geocology.ca>
2013-10-16 20:26:37 -0700

    Basic reproduction of VGS layout using Bootstrap grid.
    Things like fluid column widths and responsive images not yet even
    considered.

--- header.php
+++ header.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
+<html lang="en">
 <head>
 	<title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; Blog Archive <?php } ?> <?php wp_title(); ?></title>
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats -->
 	<meta name="description" content="A geoinformatics consultancy helping values-based organizations with analysis and communication." />
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>css/bootstrap.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Atom Feed" href="<?php bloginfo('atom_url'); ?>" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<?php wp_head(); ?>
+    <script src="<?php bloginfo('template_url'); ?>/scripts/jquery-1.10.2.js" type="text/javascript"></script>
     <script src="<?php bloginfo('template_url'); ?>/scripts/bootstrap.js" type="text/javascript"></script>
     <script src="<?php bloginfo('template_url'); ?>/scripts/softscroll.js" type="text/javascript"></script>
 	<script type='text/javascript'>
@@ -31,27 +34,28 @@
 	</script>
 </head>
 <body>
-	<div class="container_12" id="top">
-        <div id="banner">
-            <div class="grid_7" id="title">
-                <a href="<?php bloginfo('url'); ?>">
-					<!-- <object id="vanIsl" type="image/svg+xml" data="<?php bloginfo('template_url'); ?>\images\vanisl.svg" width="32" height="44"><img id="vanIsl" src='<?php bloginfo('template_url'); ?>\images\vanisl.png' /></object>-->
-					<span id="geocology">Geocology</span> <span id="research">research</span>
-                </a>
-				<!-- <a id='beta' href="<?php bloginfo('url'); ?>/beta" title="testing our new site">beta!</a> -->
+    <div class="container" id="top">
+        <div class="row">
+            <div id="banner">
+                <div class="col-md-7" id="title">
+                    <a href="<?php bloginfo('url'); ?>">
+                        <!-- <object id="vanIsl" type="image/svg+xml" data="<?php bloginfo('template_url'); ?>\images\vanisl.svg" width="32" height="44"><img id="vanIsl" src='<?php bloginfo('template_url'); ?>\images\vanisl.png' /></object>-->
+                        <span id="geocology">Geocology</span> <span id="research">research</span>
+                    </a>
+                    <!-- <a id='beta' href="<?php bloginfo('url'); ?>/beta" title="testing our new site">beta!</a> -->
+                </div>
+                <div class="col-md-5" id="blurb">
+                    <!-- GIS, geodesign and informatics consultancy working with organizations in Vancouver and the coastal region. -->
+                    <!-- An environmental and human geography consultancy helping public benefit organizations with analysis and communications. -->
+                    A geoinformatics consultancy helping values-based organizations with analysis and communication.
+                </div>
             </div>
-            <div class="grid_5 last" id="blurb">
-                <!-- GIS, geodesign and informatics consultancy working with organizations in Vancouver and the coastal region. -->
-                <!-- An environmental and human geography consultancy helping public benefit organizations with analysis and communications. -->
-                A geoinformatics consultancy helping values-based organizations with analysis and communication.
-            </div>
-        </div>
-        <div id="contactNav">
-            <div class="grid_7 contactDetail">
-                <a href="mailto:hugh@geocology.ca">hugh@geocology.ca</a>
-                <span class="contactDetailBullet">&bull;</span>
-                <span id="phoneNumber">604-440-1989</span>
-                <span class="contactDetailBullet">&bull;</span>
-                <!-- <a href="http://twitter.com/geocology">@geocology</a> -->
-                <a href="http://maps.google.com/maps?q=211+East+Georgia+Street,+Vancouver,+BC,+Canada&hl=en&ll=49.278576,-123.099434&spn=0.001904,0.009645&sll=49.278649,-123.099372&layer=c&cbp=11,347.66,,0,7.21&cbll=49.278576,-123.099434&hnear=211+E+Georgia+St,+Vancouver,+Greater+Vancouver+Regional+District,+British+Columbia+V6A+1Z7,+Canada&t=m&z=17&panoid=1T3bgakrFvhAwdCdpRpr8Q">105-211 E Georgia St Vancouver BC V6A1Z6</a>
-            </div>
+            <div id="contactNav">
+                <div class="col-md-7 contactDetail">
+                    <a href="mailto:hugh@geocology.ca">hugh@geocology.ca</a>
+                    <span class="contactDetailBullet">&bull;</span>
+                    <span id="phoneNumber">604-440-1989</span>
+                    <span class="contactDetailBullet">&bull;</span>
+                    <!-- <a href="http://twitter.com/geocology">@geocology</a> -->
+                    <a href="http://maps.google.com/maps?q=211+East+Georgia+Street,+Vancouver,+BC,+Canada&hl=en&ll=49.278576,-123.099434&spn=0.001904,0.009645&sll=49.278649,-123.099372&layer=c&cbp=11,347.66,,0,7.21&cbll=49.278576,-123.099434&hnear=211+E+Georgia+St,+Vancouver,+Greater+Vancouver+Regional+District,+British+Columbia+V6A+1Z7,+Canada&t=m&z=17&panoid=1T3bgakrFvhAwdCdpRpr8Q">105-211 E Georgia St Vancouver BC V6A1Z6</a>
+                </div>
--- index.php
+++ index.php
@@ -1,125 +1,140 @@
 <?php get_header(); ?>
-			<div class="grid_5" id="topNavLinks">
-				<h2>
-					<span class="navLink"><a href="<?php bloginfo('url'); ?>/#recentprojects" title="jump down to recent projects we've been working on" >recent projects</a> &darr;</span>
-					<span class="navLink"><a href="<?php bloginfo('url'); ?>/#about" title="jump down to some background about Geocology Research"> about us</a> &darr;</span>
-				</h2>
-			</div>
-		</div><!-- contactNav -->
-        <div class="sectionBreak"> </div>
-        <div class="grid_12" id="featuredproject">
-            <h1>featured project</h1>
-        </div>
-        <?php 
-            query_posts('post_type=geo_project&display=featured&posts_per_page=1&orderby=rand');
-            while (have_posts()) : the_post();
-        ?>
-        <div class="grid_6">
-            <a href="<?php the_permalink(); ?>">
-                <?php 
-					$img_args = array(
-						'title'	=> 'click to go to the project page',
-					);
-                    if ( has_post_thumbnail() ) {
-                    the_post_thumbnail( 'project-medium');
-                    } 
-                ?>
-            </a>
-        </div>
-        <div class="grid_6">
-            <a href="<?php the_permalink(); ?>" title="click to go to the project page">
-                <h2>
-                    <?php the_title(); ?>
-                </h2>
-            </a>
-            <div class="grid_3 alpha" id="projectDescription">
-                <?php
-                    the_excerpt();
-                    if ( $post->post_content != "" ) { ?>
-                        <span class="navLink"><a href="<?php the_permalink(); ?>" title="click to go to the project page">more</a> &rarr;</span>
-                    <?php }
-                ?>
+                <div class="col-md-5" id="topNavLinks">
+                    <span class="navLink"><a href="<?php bloginfo('url'); ?>/#recentprojects" title="jump down to recent projects we've been working on" >recent projects</a> &darr;</span>
+                    <span class="navLink"><a href="<?php bloginfo('url'); ?>/#about" title="jump down to some background about Geocology Research"> about us</a> &darr;</span>
+                </div>
+            </div><!-- contactNav -->
+        </div><!-- .row -->
+        <div class="row">
+            <div class="col-md-12" id="featuredproject">
+                <h1>featured project</h1>
             </div>
-            <div class="grid_3 omega" id="projectMeta">
-                <table>
-                    <?php 
-                        $for = get_post_meta($post->ID, 'for', true);
-                        if (!empty($for)) {
-                            echo '
-                            <tr>
-                                <td class="key">for</td>
-                                <td class="partner">';
+        </div>
+        <div class="row">
+            <?php
+                query_posts('post_type=geo_project&display=featured&posts_per_page=1&orderby=rand');
+                while (have_posts()) : the_post();
+            ?>
+            <div class="col-md-6">
+                <a href="<?php the_permalink(); ?>">
+                    <?php
+                        $img_args = array(
+                            'title'	=> 'click to go to the project page',
+                        );
+                        if ( has_post_thumbnail() ) {
+                        the_post_thumbnail( 'project-medium');
+                        }
+                    ?>
+                </a>
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-12">
+                        <a href="<?php the_permalink(); ?>" title="click to go to the project page">
+                            <h2>
+                                <?php the_title(); ?>
+                            </h2>
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6" id="projectDescription">
+                        <?php
+                        the_excerpt();
+                        if ( $post->post_content != "" ) { ?>
+                            <span class="navLink"><a href="<?php the_permalink(); ?>" title="click to go to the project page">more</a> &rarr;</span>
+                        <?php }
+                        ?>
+                    </div>
+                    <div class="col-md-6" id="projectMeta">
+                        <table>
+                            <?php
+                            $for = get_post_meta($post->ID, 'for', true);
+                            if (!empty($for)) {
+                                echo '
+                                <tr>
+                                    <td class="key">for</td>
+                                    <td class="partner">';
                                 echo $for;
                                 echo '</td>
-                            </tr>';
-                        }
-                        $with = get_post_meta($post->ID, 'with', true);
-                        if (!empty($with)) {
-                            echo '
-                            <tr>
-                                <td class="key">with</td>
-                                <td class="partner">';
+                                </tr>';
+                            }
+                            $with = get_post_meta($post->ID, 'with', true);
+                            if (!empty($with)) {
+                                echo '
+                                <tr>
+                                    <td class="key">with</td>
+                                    <td class="partner">';
                                 echo $with;
                                 echo '</td>
-                            </tr>';
-                        } 
-                        $and = get_post_meta($post->ID, 'and', true);
-                        if (!empty($and)) {
-                            echo '
-                            <tr>
-                                <td class="key">and</td>
-                                <td class="partner">';
+                                </tr>';
+                            }
+                            $and = get_post_meta($post->ID, 'and', true);
+                            if (!empty($and)) {
+                                echo '
+                                <tr>
+                                    <td class="key">and</td>
+                                    <td class="partner">';
                                 echo $and;
                                 echo '</td>
-                            </tr>';
-                        } 
-                    ?>	
-                </table>
+                                </tr>';
+                            }
+                            ?>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <?php
+                endwhile;
+                wp_reset_query()
+            ?>
+        </div>
+        <div class="row">
+            <div class="col-md-12" id="recentprojects">
+                <h1><span>recent projects</span></h1>
             </div>
         </div>
-        <?php 
-            endwhile; 
-            wp_reset_query()
-        ?>
-        <div class="grid_12" id="recentprojects">
-            <h1><span>recent projects</span></h1>
-        </div>
-        <!-- query adapted from diceone http://wordpress.org/support/topic/297090#post-1186292 and http://wordpress.org/support/topic/adding-a-different-class-to-every-third-post -->
-        <?php
-            query_posts('post_type=geo_project&display=recent&orderby=date&order=DESC&posts_per_page=4');
-            while (have_posts()) : the_post();
-        ?>
-        <div class="grid_3 projectCard">
-			<?php 
-				$excerpt = strip_tags(get_the_excerpt());
-				$excerpt = $excerpt . ' Click for more info.';
-				$img_args = array(
-					'title'	=> $excerpt,
-				);
-			?>
-            <a href="<?php the_permalink(); ?>">
+        <div class="row">
+            <!-- query adapted from diceone http://wordpress.org/support/topic/297090#post-1186292 and http://wordpress.org/support/topic/adding-a-different-class-to-every-third-post -->
             <?php
-            if ( has_post_thumbnail() ) {
-            the_post_thumbnail( 'project-thumbnail', $img_args);
-            }
+                query_posts('post_type=geo_project&display=recent&orderby=date&order=DESC&posts_per_page=4');
+                while (have_posts()) : the_post();
             ?>
-            </a>
-            <a href="<?php the_permalink(); ?>" title="<?php echo $excerpt ?>">
-                <?php the_title(); ?>
-            </a>
+            <div class="col-md-3 projectCard">
+                <?php
+                    $excerpt = strip_tags(get_the_excerpt());
+                    $excerpt = $excerpt . ' Click for more info.';
+                    $img_args = array(
+                        'title'	=> $excerpt,
+                    );
+                ?>
+                <a href="<?php the_permalink(); ?>">
+                <?php
+                if ( has_post_thumbnail() ) {
+                the_post_thumbnail( 'project-thumbnail', $img_args);
+                }
+                ?>
+                </a>
+                <a href="<?php the_permalink(); ?>" title="<?php echo $excerpt ?>">
+                    <?php the_title(); ?>
+                </a>
+            </div>
+            <?php
+                endwhile;
+                wp_reset_query()
+            ?>
+            <div class="grid_12 navLinks" id="recentProjectsNavLinks">
+                <span class="navLink"><a href="#top" title="jump back to the top of this page">top</a> &uarr;</span> <span class="navLink"><a href="<?php bloginfo('url'); ?>/projects" title="go to a page with all of our projects">more projects</a> &rarr;</span>
+            </div>
         </div>
-        <?php
-            endwhile;
-            wp_reset_query()
-        ?>
-        <div class="grid_12 navLinks" id="recentProjectsNavLinks">
-            <span class="navLink"><a href="#top" title="jump back to the top of this page">top</a> &uarr;</span> <span class="navLink"><a href="<?php bloginfo('url'); ?>/projects" title="go to a page with all of our projects">more projects</a> &rarr;</span>
+        <div class="row">
+            <div class="col-md-12" id="about">
+                <h1><span>about Geocology</span></h1>
+            </div>
         </div>
-        <div class="grid_12" id="about">
-            <h1><span>about Geocology</span></h1>
-        </div>
-        <div class="grid_6" id="whatwedo">
-            <h2>what we do</h2>
+        <div class="row">
+            <div class="col-md-6" id="whatwedo">
+                <h2>what we do</h2>
                 <p>
                     Geocology provides GIS and informatics capacity to environmental and community organizations that don't have that capacity in-house.
                 </p>
@@ -129,47 +144,55 @@
                 <p>
                     Our goal is to become a shared resource of best practices and repeatable experience that Vancouver and coastal area organizations can collectively draw on.
                 </p>
+            </div>
+            <div class="col-md-6" id="partners">
+                <h2>who we've worked with</h2>
+                <div class="row">
+                    <div class="col-md-4">
+                        <a href="http://davidsuzuki.org" title="link to the David Suzuki Foundation website">
+                            <img src="<?php bloginfo('template_url'); ?>/images/suzuki150x74.png" />
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="http://dogwoodinitiative.org" title="link to the Dogwood Initiative website">
+                            <img src="<?php bloginfo('template_url'); ?>/images/dogwood150x74.png" />
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="http://www.elizabethmay.ca" title="link to the Elizabeth May campaign website">
+                            <img src="<?php bloginfo('template_url'); ?>/images/gpc150x74.png" />
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="http://geomemes.com" title="link to the Geomemes Research website">
+                            <img src="<?php bloginfo('template_url'); ?>/images/geomemes150x74.png" />
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="http://echotrack.com" title="link to the Echotrack Inc website">
+                            <img src="<?php bloginfo('template_url'); ?>/images/echotrack150x74.png" />
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="http://www.mcallister-research.com" title="link to the McCallister Opinion Research website">
+                            <img src="<?php bloginfo('template_url'); ?>/images/mcallister150x74.png" />
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        We love working with these people. These are awesome people.
+                    </div>
+                </div>
+            </div>
+        </div class="row">
+        <div class="row" id="staff">
+            <div class="col-md-12">
+                <h2>who we are</h2>
+            </div>
         </div>
-        <div class="grid_6" id="partners">
-            <h2>who we've worked with</h2>
-            <div class="grid_2 alpha">
-                <a href="http://davidsuzuki.org" title="link to the David Suzuki Foundation website">
-                    <img src="<?php bloginfo('template_url'); ?>/images/suzuki150x74.png" />
-                </a>
-            </div>
-            <div class="grid_2">
-                <a href="http://dogwoodinitiative.org" title="link to the Dogwood Initiative website">
-                    <img src="<?php bloginfo('template_url'); ?>/images/dogwood150x74.png" />
-                </a>
-            </div>
-            <div class="grid_2 omega">
-                <a href="http://www.elizabethmay.ca" title="link to the Elizabeth May campaign website">
-                    <img src="<?php bloginfo('template_url'); ?>/images/gpc150x74.png" />
-                </a>
-            </div>
-            <div class="grid_2 alpha">
-                <a href="http://geomemes.com" title="link to the Geomemes Research website">
-                    <img src="<?php bloginfo('template_url'); ?>/images/geomemes150x74.png" />
-                </a>
-            </div>
-            <div class="grid_2">
-                <a href="http://echotrack.com" title="link to the Echotrack Inc website">
-                    <img src="<?php bloginfo('template_url'); ?>/images/echotrack150x74.png" />
-                </a>
-            </div>
-            <div class="grid_2 omega">
-                <a href="http://www.mcallister-research.com" title="link to the McCallister Opinion Research website">
-                    <img src="<?php bloginfo('template_url'); ?>/images/mcallister150x74.png" />
-                </a>
-            </div>
-            <br />
-            <p>
-                We love working with these people. These are awesome people.
-            </p>
-        </div>
-        <div class="grid_12" id="staff">
-            <h2>who we are</h2>
-            <div class="grid_3 alpha meta">
+        <div class="row">
+            <div class="col-md-3 meta">
                 <img src="<?php bloginfo('template_url'); ?>/images/hughAuric3.jpg" />
                 <h3>Hugh Stimson</h3>
                 environmental geographer<br />
@@ -178,7 +201,7 @@
                 <a href="http://twitter.com/hughstimson">@hughstimson</a><br />
                 604.440.1989<br />
             </div>
-            <div class="grid_3">
+            <div class="col-md-3">
                 <p>
                     Hugh studied ecology at the University of Guelph before working in conservation GIS and remote sensing at UC Davis and the Smithsonian Institution.
                 </p>
@@ -189,7 +212,7 @@
                     Hugh used to like motorcycles and deserts but now likes camper vans and fjords. He has planted over a million trees by hand. True fact.
                 </p>
             </div>
-            <div class="grid_3 meta">
+            <div class="col-md-3 meta">
                 <img src="<?php bloginfo('template_url'); ?>/images/janeBarnacles.jpg" />
                 <h3>Jane Boles</h3>
                 human geographer<br />
@@ -198,15 +221,19 @@
                 <a href="https://twitter.com/bolesfin">@bolesfin</a><br />
                 604.603.6764
             </div>
-            <div class="grid_3 omega">
-				<p>Jane works with forest dependent communities on climate change and participatory natural resource governance.</p> 
-				<p>For the last several years she has worked in the Congo basin leveraging REDD and other carbon programs towards climate change mitigation/adaptation and livelihood enhancement.</p>
-				<p>Jane studied Geography and Political Science at Concordia University, and is endlessly grateful to do work that involves good measures of reading, writing, talking, and stomping around in the muck.</p>
+            <div class="col-md-3">
+                <p>Jane works with forest dependent communities on climate change and participatory natural resource governance.</p>
+                <p>For the last several years she has worked in the Congo basin leveraging REDD and other carbon programs towards climate change mitigation/adaptation and livelihood enhancement.</p>
+                <p>Jane studied Geography and Political Science at Concordia University, and is endlessly grateful to do work that involves good measures of reading, writing, talking, and stomping around in the muck.</p>
             </div>
         </div>
-        <div class="grid_12" id="location">
-            <h2>where we are</h2>
-            <div class="grid_3 alpha">
+        <div class="row">
+            <div class="col-12-md" id="location">
+                <h2>where we are</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
                 <p>
                     East Vancouver.<br />
                     Chinatown counts as East Van right?
@@ -222,11 +249,13 @@
                     But it sure would be nice to get back out into the field one of these days.
                 </p>
             </div>
-            <div class="grid_9 omega">
-				<iframe width='100%' height='400' frameBorder='0' src='http://a.tiles.mapbox.com/v3/hughstimson.map-ispdkxge.html#12/49.27321610028051/-123.13697528076172'></iframe>
+            <div class="col-md-9">
+                <iframe width='100%' height='400' frameBorder='0' src='http://a.tiles.mapbox.com/v3/hughstimson.map-ispdkxge.html#12/49.27321610028051/-123.13697528076172'></iframe>
             </div>
         </div>
-        <div class="grid_12 navLinks">
-            <span class="navLink"><a href="#top" title="jump back to the top of this page">top</a> &uarr;</span>
+        <div class="row">
+            <div class="col-md-12 navLinks">
+                <span class="navLink"><a href="#top" title="jump back to the top of this page">top</a> &uarr;</span>
+            </div>
         </div>
 		<?php get_footer(); ?>
